9aa254cfb6b55b44cda09b98bd1da434aa94a013
frannoel <frannoeloliveira@gmail.com>
2018-06-25 17:54:16 -0300

    atualizando

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.ComboBaud = new System.Windows.Forms.ComboBox();
+			this.ComboPorta = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Button1 = new System.Windows.Forms.Button();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.SP = new System.IO.Ports.SerialPort(this.components);
+			this.timerx = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// ComboBaud
+			// 
+			this.ComboBaud.FormattingEnabled = true;
+			this.ComboBaud.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+			this.ComboBaud.Location = new System.Drawing.Point(67, 71);
+			this.ComboBaud.Name = "ComboBaud";
+			this.ComboBaud.Size = new System.Drawing.Size(110, 21);
+			this.ComboBaud.TabIndex = 1;
+			this.ComboBaud.SelectedIndexChanged += new System.EventHandler(this.ComboBaud_SelectedIndexChanged);
+			// 
+			// ComboPorta
+			// 
+			this.ComboPorta.FormattingEnabled = true;
+			this.ComboPorta.Location = new System.Drawing.Point(67, 44);
+			this.ComboPorta.Name = "ComboPorta";
+			this.ComboPorta.Size = new System.Drawing.Size(110, 21);
+			this.ComboPorta.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(17, 48);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Porta";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 74);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Baud Rate";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(53, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(49, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Umidade";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(147, 32);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(90, 13);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Temperatura (°C )";
+			this.label4.Click += new System.EventHandler(this.label4_Click);
+			// 
+			// Button1
+			// 
+			this.Button1.Location = new System.Drawing.Point(58, 110);
+			this.Button1.Name = "Button1";
+			this.Button1.Size = new System.Drawing.Size(75, 23);
+			this.Button1.TabIndex = 2;
+			this.Button1.Text = "Conectar/Desconectar";
+			this.Button1.UseVisualStyleBackColor = true;
+			this.Button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// chart1
+			// 
+			chartArea1.AxisX.IsLabelAutoFit = false;
+			chartArea1.AxisY.IsLabelAutoFit = false;
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(23, 171);
+			this.chart1.Name = "chart1";
+			series1.BorderWidth = 2;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Legend = "Legend1";
+			series1.Name = "Umidade";
+			series2.BorderWidth = 2;
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series2.Legend = "Legend1";
+			series2.Name = "Temperatura";
+			this.chart1.Series.Add(series1);
+			this.chart1.Series.Add(series2);
+			this.chart1.Size = new System.Drawing.Size(576, 139);
+			this.chart1.TabIndex = 6;
+			this.chart1.Text = "chart1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(36, 48);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(83, 71);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(150, 48);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(86, 71);
+			this.textBox2.TabIndex = 7;
+			this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.ComboPorta);
+			this.groupBox1.Controls.Add(this.ComboBaud);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.Button1);
+			this.groupBox1.Location = new System.Drawing.Point(23, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(177, 153);
+			this.groupBox1.TabIndex = 11;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Portas";
+			this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Location = new System.Drawing.Point(206, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(393, 153);
+			this.groupBox2.TabIndex = 10;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Leituras";
+			this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(263, 32);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(89, 13);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Temperatura (°F )";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(266, 48);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(86, 71);
+			this.textBox3.TabIndex = 9;
+			this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+			// 
+			// SP
+			// 
+			this.SP.PortName = "COM3";
+			this.SP.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SP_DataReceived);
+			// 
+			// timerx
+			// 
+			this.timerx.Interval = 500;
+			this.timerx.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.chart1);
+			this.DoubleBuffered = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Sensor de umidade e temperatura";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox ComboBaud;
+		private System.Windows.Forms.ComboBox ComboPorta;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button Button1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.IO.Ports.SerialPort SP;
+		private System.Windows.Forms.Timer timerx;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox3;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		Thread X;
+		public Form1()
+		{
+			InitializeComponent();
+			ComboPorta.DataSource = SerialPort.GetPortNames();
+			X = new Thread(new ThreadStart(Serial));
+		}
+
+		private void atualizaListaCOMs()
+		{
+			int i;
+			bool quantDiferente;    //flag para sinalizar que a quantidade de portas mudou
+
+			i = 0;
+			quantDiferente = false;
+
+			//se a quantidade de portas mudou
+			if (ComboPorta.Items.Count == SerialPort.GetPortNames().Length)
+			{
+				foreach (string s in SerialPort.GetPortNames())
+				{
+					if (ComboPorta.Items[i++].Equals(s) == false)
+					{
+						quantDiferente = true;
+					}
+				}
+			}
+			else
+			{
+				quantDiferente = true;
+			}
+
+			//Se não foi detectado diferença
+			if (quantDiferente == false)
+			{
+				return;                     //retorna
+			}
+
+			//limpa comboBox
+			ComboPorta.Items.Clear();
+
+			//adiciona todas as COM diponíveis na lista
+			foreach (string s in SerialPort.GetPortNames())
+			{
+				ComboPorta.Items.Add(s);
+			}
+			//seleciona a primeira posição da lista
+			ComboPorta.SelectedIndex = 0;
+		}
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			if (SP.IsOpen)
+			{
+				SP.Close();
+				X.Abort();
+				ComboPorta.Enabled = true;
+				timerx.Enabled = false;
+				
+			}
+			else
+			{
+				SP.PortName = ComboPorta.Text;
+				SP.BaudRate = Convert.ToInt32(ComboBaud.Text);
+				SP.Open();
+				//X.Start();
+				timerx.Enabled = true;
+
+			}
+		}
+
+		void Serial()
+		{
+			while (true)
+			{
+				SP.Write("a");
+				textBox1.Text += SP.ReadLine();
+				textBox1.Text += "\r\n";
+			}
+		}
+
+		private void groupBox1_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void SP_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+			this.Invoke(new EventHandler(transfere));
+		}
+
+		void transfere(object sender, EventArgs e)
+		{
+			string dado = SP.ReadLine();
+			string[] dados = dado.Split(',');
+			textBox1.Text += dados[0];
+			textBox1.Text += "%\r\n";
+			textBox1.Select(textBox1.Text.Length, 0);
+			textBox1.ScrollToCaret();
+
+			textBox2.Text += dados[1];
+			textBox2.Text += "°C\r\n";
+			textBox2.Select(textBox2.Text.Length, 0);
+			textBox2.ScrollToCaret();
+
+			textBox3.Text += dados[2];
+			textBox3.Text += "°F\r\n";
+			textBox3.Select(textBox3.Text.Length, 0);
+			textBox3.ScrollToCaret();
+
+			chart1.Series[0].Points.Add(Convert.ToDouble(dados[0]));
+			chart1.Series[1].Points.Add(Convert.ToDouble(dados[1]));
+			int x = chart1.Series[0].Points.Count;
+			if (x > 100)
+				chart1.ChartAreas[0].AxisX.ScaleView.Zoom(x - 100, x);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			SP.Write("a");
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label4_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void groupBox2_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			SP.Close();
+			X.Abort();
+			timerx.Enabled = false;
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ComboBaud_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
